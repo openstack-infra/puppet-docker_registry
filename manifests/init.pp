41c94ea1b3516d2877496cd2968b3a2c3bee9dae
Michal (inc0) Jastrzebski <inc007@gmail.com>
2016-11-17 20:13:31 +0000

    Cookiecutter commit
    Initial commit from cookiecutter
    
    Change-Id: Ie08f9806d95e5cf77658d63af77fbde695f0d02a

---
+++ manifests/init.pp
@@ -0,0 +1,10 @@
+# == Class: docker_registry
+#
+# Full description of class docker_registry here.
+#
+# === Parameters
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#
+class docker_registry {}
